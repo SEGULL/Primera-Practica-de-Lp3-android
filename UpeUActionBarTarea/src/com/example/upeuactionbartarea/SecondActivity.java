889fbda8035fe3e3685fc871a627bc95c7d051f4
SEGULL <alejandro.canahuire.hilari@github.com>
2014-05-20 11:01:29 -0500

    tarea subida ingeniero

---
+++ UpeUActionBarTarea/src/com/example/upeuactionbartarea/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.upeuactionbartarea;
+
+import com.example.upeuactionbartarea.BaseMenuActivity;
+import com.example.upeuactionbartarea.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SecondActivity extends BaseMenuActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+	}
+}
